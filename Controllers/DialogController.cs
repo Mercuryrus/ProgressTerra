9906634851a485067375ff0a7dc64069e476ad5a
Илья Аникин <mercury2612198@gmail.com>
2021-07-19 20:39:51 +0300

    Добавьте файлы проекта.

---
+++ Controllers/DialogController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProgressTerra.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgressTerra.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FindIDRGDialogController : ControllerBase
+    {
+        [HttpPost]
+        public List<Guid> Post(IList<Guid> guids)
+        {
+            var dialogs = new RGDialogsClients().Init();
+
+            Dictionary<Guid, List<Guid>> chats = new();
+            foreach (RGDialogsClients dialog in dialogs)
+            {
+                if (!chats.ContainsKey(dialog.IDRGDialog))
+                {
+                    chats[dialog.IDRGDialog] = new();
+                }
+                chats[dialog.IDRGDialog].Add(dialog.IDClient);
+            }
+
+            List<Guid> result = new();
+
+            foreach (KeyValuePair<Guid, List<Guid>> item in chats)
+            {
+                bool isContain = true;
+                foreach (Guid guid in guids)
+                {
+                    if (!item.Value.Contains(guid))
+                    {
+                        isContain = false;
+                        break;
+                    }
+                }
+                if (isContain)
+                {
+                    result.Add(item.Key);
+                }
+            }
+
+
+            if (result.Count>0)
+            return result;
+            else
+            return new() { new Guid() };
+        }
+    }
+}
